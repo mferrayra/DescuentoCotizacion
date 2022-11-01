881a88a6245dffaa93e08fa35843e7f1d406abbf
mferrayra <mferrayra@sancristobalsf.com.ar>
2022-11-01 19:44:17 -0300

    Firs commit

---
+++ src/main/java/com/sc/model/Cotizacion.java
@@ -0,0 +1,41 @@
+package com.sc.model;
+
+public class Cotizacion {
+	private String zona;
+	private String modelo;
+	private double cotizacion;
+	private float descuento;
+	private double total;
+	public String getZona() {
+		return zona;
+	}
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public double getCotizacion() {
+		return cotizacion;
+	}
+	public void setCotizacion(double cotizacion) {
+		this.cotizacion = cotizacion;
+	}
+	public float getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+		if (this.cotizacion > 0 && this.descuento > 0)
+			this.total = this.cotizacion - (this.cotizacion * this.descuento / 100);
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+}
